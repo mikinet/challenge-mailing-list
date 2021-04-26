149a77cdfe739e587942551d009d9b4ae9ead0eb
Michael <antenehnigussie1@yahoo.com>
2021-04-26 10:55:22 +0100

    Completed the challenge (except the bonus). Added new routes to get, delete or update/create single mailing list. Also included the missing readme file (README.md).

--- server.js
+++ server.js
@@ -23,7 +23,49 @@ app.get("/lists", (req, res) => {
   res.status(200).send(listsArray);
 });
 
-// SUPPLEMENTARY FUNCTIONS
+// get a single list
+app.get("/lists/:name", (req, res) => {
+  const name = req.params.name;
+  if (lists.has(name)) {
+    const result = { name: name, members: lists.get(name) };
+    return res.status(200).json(result);
+  }
+  res.sendStatus(404);
+});
+
+// delete a single list
+app.delete("/lists/:name", (req, res) => {
+  const name = req.params.name;
+  if (lists.has(name)) {
+    lists.delete(name);
+    return res.status(204);
+  }
+  res.sendStatus(404);
+});
+
+// update a single list
+app.use(express.json()); // restrict the received data format to JSON
+app.put("/lists/:name", (req, res) => {
+  const name = req.params.name;
+  const incomingList = req.body;
+  
+  // check if "name" value in the request body matches to the parameter's value
+  if (name !== incomingList.name) {
+    return res.sendStatus(400); // bad request
+  }
+
+  if (lists.has(name)) {
+    // if the requested list exists, update its value
+    const list = lists.get(name);
+    list.name = req.body.name;
+    return res.status(200);
+  }
+  // otherwise create a new list
+  addToList(req.body);
+  res.sendStatus(201);
+});
+
+// add a mailing list to the existing lists
 function addToList(item) {
   lists.set(item.name, item.members);
 }
